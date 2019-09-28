630d4b4bcff16aae4c6ff0293ad89d4c17ad86d1
Craig Giles <craig.giles@creditkarma.com>
2019-09-27 17:14:11 -0700

    New install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "simlink configuration files"
+ln -s ~/Development/gilesc-misc/bashrc ~/.bash_profile
+ln -s ~/Development/gilesc-misc/tmux.conf ~/.tmux.conf
+ln -s ~/.vim/vimrc ~/.vimrc
+
+echo "Installing homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Installing tools via homebrew"
+brew install reattach-to-user-namespace
+brew install ripgrep
+brew install fzf
+/usr/local/opt/fzf/install
